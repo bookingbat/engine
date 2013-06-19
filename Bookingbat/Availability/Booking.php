c1a9be981308903d40e88a6a3054524a7ac25ae4
josh <josh.ribakoff@gmail.com>
2013-06-19 00:13:14 -0400

    Removed application specific logic out of this framework

--- Bookingbat/Availability/Booking.php
+++ Bookingbat/Availability/Booking.php
@@ -41,34 +41,5 @@ class Booking
         return isset($this->options['user_id']) ? $this->options['user_id'] : null;
     }
 
-    function allowCancelLostByUser()
-    {
-        return !$this->completed();
-    }
 
-    function allowCancelByUser()
-    {
-        $today = strtotime($this->options['today']);
-        $booking = strtotime($this->options['date']);
-
-        if ($today > $booking) {
-            return false;
-        }
-
-        $difference = ($booking - $today) / 60 / 60;
-        if ($difference >= 24) {
-            return true;
-        }
-        return false;
-    }
-
-    function completed()
-    {
-        $today = strtotime($this->options['today']);
-        $booking = strtotime($this->options['date']);
-        if ($today > $booking) {
-            return true;
-        }
-        return false;
-    }
 }
